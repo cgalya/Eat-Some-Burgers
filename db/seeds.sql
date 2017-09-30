35ea3a4ca30818aa29618578ba76555d4e8f15f4
Claire Galyaa <claire.galya@gmail.com>
2017-09-29 22:55:39 -0400

    setup done

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Dave');
+INSERT INTO burgers (burger_name) VALUES ('Georgina');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Monster', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Andes', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Ostrich', true);
+INSERT INTO burgers (burger_name) VALUES ('Jerk');
